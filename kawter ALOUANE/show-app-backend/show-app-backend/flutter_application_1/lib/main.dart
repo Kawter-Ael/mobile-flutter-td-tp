728989829105a11879000e99d13debe1220073bc
Kawter-Ael <kawteralouane123@gmail.com>
2025-04-06 01:57:51 +0000

    Add files via upload

--- kawter ALOUANE/show-app-backend/show-app-backend/flutter_application_1/lib/main.dart
+++ kawter ALOUANE/show-app-backend/show-app-backend/flutter_application_1/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'screens/login_page.dart';
 
 void main() {
-  runApp(const ShowApp() as Widget);
+  runApp(const ShowApp());
 }
- 
+
 class ShowApp extends StatelessWidget {
   const ShowApp({super.key});
 
@@ -17,10 +17,6 @@ class ShowApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const LoginPage(),
-
-      
     );
   }
 }
-
-// Removed the redefinition of StatelessWidget as it conflicts with Flutter's built-in class.
